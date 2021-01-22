44ef7789719c1e1671b94f65c823ec7d01db96f0
kouhei25k <kouhei25k@gmail.com>
2021-01-22 22:25:09 +0900

    debug

--- animation_assistance/settings.py
+++ animation_assistance/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SITE_ID=1
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
